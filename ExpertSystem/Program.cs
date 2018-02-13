e55962b84fb7baca43c3a3242f45426105f0eda2
ArturGiniyatov <artur_giniyatov@mail.ru>
2018-02-13 11:00:04 +0300

    Version 2
    Add cancer logo

--- ExpertSystem/Program.cs
+++ ExpertSystem/Program.cs
@@ -17,6 +17,7 @@ namespace ExpertSystem
             Console.WriteLine("||                                 ||");
             Console.WriteLine("||   Прототип экспертной системы   ||");
             Console.WriteLine("||                                 ||");
+            Console.WriteLine("||           ()_0,,,0_()           ||");
             Console.WriteLine("=====================================");
             Console.WriteLine("");
             Console.WriteLine("-------------press Enter-------------");
